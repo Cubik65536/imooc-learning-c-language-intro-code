b3299a2470981755e1ce017357dca1817c28f59f
KayshaTown <KayshaTown@users.noreply.github.com>
2020-11-11 15:26:22 -0500

    Initial Commit

---
+++ imooc-learning-c-language-intro-code/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  imooc-learning-c-language-intro-code
+//
+//  Created by Qian Qian on 2020-11-11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
